a81abe3cc0c2347bd0ef2d464daa86ce877e6b46
stheart76 <54047693+stheart76@users.noreply.github.com>
2023-03-21 15:17:15 +0900

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello, World!<BR>Live Update Test!";
+		return "Hello, World!<BR><BR>Live Update Test!";
 	}
 
 }
